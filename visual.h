0aa7ba15a730053df4d3e4f419610a6153a8eb95
DarkSamus669 <srivastavashivansh74@gmail.com>
2023-06-17 12:49:01 +0530

    Use multi-line comment instead

--- visual.h
+++ visual.h
@@ -1,25 +1,27 @@
-// Tiny single header C lib for simple ANSI escape codes usage.
-// It's really simple, but can save you a little time and make
-// your code look a bit nicer.
-//
-// Repository: github.com/t3mp14r3/visual
-//
-// Not all options can be set together
-//
-// To reset everything you can use RESET sequence of
-// RESETF function
-//
-// There are no guranties, that this lib will fully work in
-// work in your terminal, please, test it out before using.
-//
-// Also, please include stdio.h to make it work!
+/* Tiny single header C lib for simple ANSI escape codes usage.
+ * It's really simple, but can save you a little time and make
+ * your code look a bit nicer.
+ *
+ * Repository: github.com/t3mp14r3/visual
+ *
+ * Not all options can be set together
+ *
+ * To reset everything you can use RESET sequence of
+ * RESETF function
+ *
+ * There are no guranties, that this lib will fully work in
+ * work in your terminal, please, test it out before using.
+ *
+ * Also, please include stdio.h to make it work!
+ */
 
-// Escape sequence for ANSI codes
+/* Escape sequence for ANSI codes */
 #define ESCAPE "\x1b"
 
-// Text attributes
-// Keep in mind, that when an attribute is set, it will be
-// applied to everything, until reseted.
+/* Text attributes
+ * Keep in mind, that when an attribute is set, it will be
+ * applied to everything, until reseted.
+ */
 #define BOLD    ESCAPE "[1m"
 #define DIM     ESCAPE "[2m"
 #define ITALIC  ESCAPE "[3m"
@@ -28,24 +30,24 @@
 #define INVERSE ESCAPE "[7m"
 #define STRIKE  ESCAPE "[8m"
 
-// Text 256 colors
+/* Text 256 colors */
 #define FG_256(code) printf(ESCAPE "[38;5;" #code "m")
 #define BG_256(code) printf(ESCAPE "[48;5;" #code "m")
 
-// Text RGB colors
+/* Text RGB colors */
 #define FG_RGB(r, g, b) printf(ESCAPE "[38;2;" #r ";" #g ";" #b "m")
 #define BG_RGB(r, g, b) printf(ESCAPE "[48;2;" #r ";" #g ";" #b "m")
 
-// Disable colors
+/* Disable colors */
 #define FG_DISABLE() printf(ESCAPE "[39m")
 #define BG_DISABLE() printf(ESCAPE "[49m")
 
-// Terminal utilities
+/* Terminal utilities */
 #define CLEAR() printf(ESCAPE "c")
 #define RESET   ESCAPE "[0m" ESCAPE "[49m"
 #define RESETF() printf(ESCAPE "[0m" ESCAPE "[49m")
 #define OUTPUT_HIDE() printf(ESCAPE "^")
 
-// Cursor attributes
+/* Cursor attributes */
 #define CURSOR_HIDE() printf(ESCAPE "[?25l")
 #define CURSOR_SHOW() printf(ESCAPE "[?25h")
